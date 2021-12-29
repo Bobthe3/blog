13a418f67601cf51f30c38d22a010318ee16ff07
Devan velji <devanvelji@gmail.com>
2021-12-28 18:07:08 -0800

    added favicons

--- _site/index.js
+++ _site/index.js
@@ -1,37 +1,51 @@
-//simple_calc.html part
-let num1 = 3;
-let num2 = 3;
-let sumEL = document.getElementById("sum-el");
+// //simple_calc.html part
+// let num1 = 3;
+// let num2 = 3;
+// let sumEL = document.getElementById("sum-el");
 
-function add(){
-    result = num1+ num2 
-    sumEL.textContent = result
-}
+// function add(){
+//     result = num1+ num2 
+//     sumEL.textContent = result
+// }
 
-function sub(){
-    sumEL.textContent = num1 - num2
-}
+// function sub(){
+//     sumEL.textContent = num1 - num2
+// }
 
-function div(){
-    sumEL.textContent = num1 / num2
-}
+// function div(){
+//     sumEL.textContent = num1 / num2
+// }
 
-function mult(){
-    sumEL.textContent = num1 * num2
-}
+// function mult(){
+//     sumEL.textContent = num1 * num2
+// }
 
-let sumEl = document.getElementById("sum-el")
+// let sumEl = document.getElementById("sum-el")
 
-function add() {
+// function add() {
     
-    sumEl.textContent = "Sum: " + num1 + num2
-}
+//     sumEl.textContent = "Sum: " + num1 + num2
+// }
 
-/// todo list code
-let click = document.getElementById("in");
-let list1 = document.getElementById("list");
-let btn = document.getElementById("btn")
-btn.addEventListener('click', function addTodo(){
-    var paragraph = document.createElement('p')
-    list1.appendChild(paragraph)
-})
+// /// todo list code
+// let click = document.getElementById("in");
+// let list1 = document.getElementById("list");
+// let btn = document.getElementById("btn")
+// btn.addEventListener('click', function addTodo(){
+//     var paragraph = document.createElement('p')
+//     list1.appendChild(paragraph)
+// })
+
+// let yourName;
+// do {
+//     yourName = prompt("Who are you?"); } 
+// while (!yourName); console.log(yourName);
+
+
+// for(let x=0; x<7; x=x+1)
+// {
+//     for(let y=1; x>=y; y=y+1)
+//     {
+//         console.log("3")
+//     }
+// }
