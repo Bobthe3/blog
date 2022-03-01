d8d25fbd27b12e50e2d831b94f034a26eabf4c2c
Devan velji <devanvelji@gmail.com>
2022-03-01 13:53:12 -0800

    small update

--- _site/index.js
+++ _site/index.js
@@ -1,55 +1,67 @@
-// //simple_calc.html part
-// let num1 = 3;
-// let num2 = 3;
-// let sumEL = document.getElementById("sum-el");
+// Import the functions you need from the SDKs you need
 
-// function add(){
-//     result = num1+ num2 
-//     sumEL.textContent = result
-// }
+import { initializeApp } from "firebase/app";
 
-// function sub(){
-//     sumEL.textContent = num1 - num2
-// }
+import { getAnalytics } from "firebase/analytics";
 
-// function div(){
-//     sumEL.textContent = num1 / num2
-// }
+import { getAuth } from "firebase/auth";
 
-// function mult(){
-//     sumEL.textContent = num1 * num2
-// }
+import { getFirestore } from "firebase/firestore";
 
-// let sumEl = document.getElementById("sum-el")
+const firebaseConfig = {
 
-// function add() {
-    
-//     sumEl.textContent = "Sum: " + num1 + num2
-// }
+  apiKey: "AIzaSyALkRoDZcYWniGiDP8i4HXIP46RNeGtVnI",
 
-// /// todo list code
-// let click = document.getElementById("in");
-// let list1 = document.getElementById("list");
-// let btn = document.getElementById("btn")
-// btn.addEventListener('click', function addTodo(){
-//     var paragraph = document.createElement('p')
-//     list1.appendChild(paragraph)
-// })
+  authDomain: "devanveljicom.firebaseapp.com",
 
-// let yourName;
-// do {
-//     yourName = prompt("Who are you?"); } 
-// while (!yourName); console.log(yourName);
+  projectId: "devanveljicom",
+
+  storageBucket: "devanveljicom.appspot.com",
+
+  messagingSenderId: "581400374067",
+
+  appId: "1:581400374067:web:c3d6e19f968aa9660aac46",
+
+  measurementId: "G-JSP9VW35TW"
+
+};
 
 
-// for(let x=0; x<7; x=x+1)
-// {
-//     for(let y=1; x>=y; y=y+1)
-//     {
-//         console.log("3")
-//     }
-// }
+// Initialize Firebase
 
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = firebase.auth();
+const db = firebase. firestore(); 
+
+
+
+
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+
+  firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+
+///-------------------- old
 function pasuser(form) { 
     if (form.id.value=="ashwin"||form.id.value=="bupin"||form.id.value=="varun"||form.id.value=="ness") { 
         if (form.pass.value=="konda") { location="assets/other/ogga_boga.mp4" } 
@@ -60,3 +72,4 @@ function pasuser(form) {
         } 
         else { alert("Wrong Username") } 
     } 
+
